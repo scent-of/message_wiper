a8690532d5f845061ad212accde52c3ed7cbb74a
scent-of <koyfreee@gmail.com>
2024-07-23 12:00:35 +0900

    bot.py

--- bot.py
+++ bot.py
@@ -13,7 +13,7 @@ intents.message_content = True  # 메시지 내용을 읽기 위해 추가
 bot = commands.Bot(command_prefix="!", intents=intents)
 
 # 메시지를 삭제할 특정 채널 ID
-TARGET_CHANNEL_ID = 1264999351923773531  # 여기에 삭제할 채널의 ID를 입력하세요.
+TARGET_CHANNEL_ID = 123456789012345678  # 여기에 삭제할 채널의 ID를 입력하세요.
 
 async def clean_channel(channel):
     # 채널의 모든 메시지 가져오기
@@ -36,7 +36,7 @@ async def on_ready():
     
     # 봇이 로그인될 때 특정 채널의 메시지 정리
     for guild in bot.guilds:
-        channel = guild.get_channel(1264999351923773531)
+        channel = guild.get_channel(TARGET_CHANNEL_ID)
         if channel:
             await clean_channel(channel)
 
@@ -58,5 +58,4 @@ async def on_message(message):
     # 다른 명령어 처리와 충돌하지 않도록 on_message를 오버라이드 할 때 필요
     await bot.process_commands(message)
 
-access_token = os.environ["BOT_TOKEN"]
-bot.run(access_token)
+bot.run(os.getenv('DISCORD_TOKEN'))
